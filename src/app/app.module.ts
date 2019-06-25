f851645a8bd93729fa87517b8d86b751d94a8eeb
samgill172 <robertwilk916@gmail.com>
2019-06-25 13:39:18 -0700

    added ftext

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FancyTextComponent } from './fancy-text/fancy-text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FancyTextComponent
   ],
   imports: [
     BrowserModule
